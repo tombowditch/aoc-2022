6e853fc35f58d8eb7d9ee9e470eba8105469a730
Tommy Bowditch <tommy.bowditch@nandos.co.uk>
2022-12-02 09:53:32 +0000

    day 2

---
+++ day-2/src/main.rs
@@ -0,0 +1,228 @@
+fn main() {
+    // part_one();
+
+    part_two();
+}
+
+fn part_one() {
+    let input = include_str!("../input.txt")
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect::<Vec<&str>>();
+
+    let mut score = 0;
+
+    for r in input {
+        let (p1r, p2r) = r.split_once(" ").unwrap();
+        // conver to char
+        let p1 = p1r.chars().next().unwrap();
+        let p2 = p2r.chars().next().unwrap();
+
+        println!("player one '{}', player two '{}'", p1, p2);
+
+        // shape score
+        score += match p2 {
+            'X' => 1,
+            'Y' => 2,
+            'Z' => 3,
+            _ => 0,
+        };
+
+        match p1 {
+            'A' => {
+                // rock
+                match p2 {
+                    // rock
+                    'X' => {
+                        // draw
+                        score += 3;
+                    }
+                    // paper
+                    'Y' => {
+                        // win
+                        score += 6;
+                    }
+                    // scissors
+                    'Z' => {
+                        // lose
+                        score += 0;
+                    }
+                    _ => {}
+                }
+            }
+            'B' => {
+                // paper
+                match p2 {
+                    // rock
+                    'X' => {
+                        // lose
+                        score += 0;
+                    }
+                    // paper
+                    'Y' => {
+                        // draw
+                        score += 3;
+                    }
+                    // scissors
+                    'Z' => {
+                        // win
+                        score += 6;
+                    }
+                    _ => {}
+                }
+            }
+            'C' => {
+                // scissors
+                match p2 {
+                    // rock
+                    'X' => {
+                        // win
+                        score += 6;
+                    }
+                    // paper
+                    'Y' => {
+                        // lose
+                        score += 0;
+                    }
+                    // scissors
+                    'Z' => {
+                        // draw
+                        score += 3;
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("score: {}", score);
+}
+
+fn part_two() {
+    let input = include_str!("../input.txt")
+        .trim_end_matches("\n")
+        .split("\n")
+        .collect::<Vec<&str>>();
+
+    let mut score = 0;
+
+    for r in input {
+        let (p1r, p2r) = r.split_once(" ").unwrap();
+        // conver to char
+        let p1 = p1r.chars().next().unwrap();
+        let mut p2 = p2r.chars().next().unwrap();
+
+        println!("player one '{}', player two '{}'", p1, p2);
+
+        // A = rock
+        // B = paper
+        // C = scissors
+        //
+        // X = rock
+        // Y = paper
+        // Z = scissors
+
+        p2 = match p2 {
+            // lose
+            'X' => match p1 {
+                'A' => 'Z',
+                'B' => 'X',
+                'C' => 'Y',
+                _ => '_',
+            },
+            // draw
+            'Y' => match p1 {
+                'A' => 'X',
+                'B' => 'Y',
+                'C' => 'Z',
+                _ => '_',
+            },
+            // win
+            'Z' => match p1 {
+                'A' => 'Y',
+                'B' => 'Z',
+                'C' => 'X',
+                _ => '_',
+            },
+            _ => '_',
+        };
+
+        // shape score
+        score += match p2 {
+            'X' => 1,
+            'Y' => 2,
+            'Z' => 3,
+            _ => 0,
+        };
+
+        match p1 {
+            'A' => {
+                // rock
+                match p2 {
+                    // rock
+                    'X' => {
+                        // draw
+                        score += 3;
+                    }
+                    // paper
+                    'Y' => {
+                        // win
+                        score += 6;
+                    }
+                    // scissors
+                    'Z' => {
+                        // lose
+                        score += 0;
+                    }
+                    _ => {}
+                }
+            }
+            'B' => {
+                // paper
+                match p2 {
+                    // rock
+                    'X' => {
+                        // lose
+                        score += 0;
+                    }
+                    // paper
+                    'Y' => {
+                        // draw
+                        score += 3;
+                    }
+                    // scissors
+                    'Z' => {
+                        // win
+                        score += 6;
+                    }
+                    _ => {}
+                }
+            }
+            'C' => {
+                // scissors
+                match p2 {
+                    // rock
+                    'X' => {
+                        // win
+                        score += 6;
+                    }
+                    // paper
+                    'Y' => {
+                        // lose
+                        score += 0;
+                    }
+                    // scissors
+                    'Z' => {
+                        // draw
+                        score += 3;
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("score: {}", score);
+}
